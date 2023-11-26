141c2f847bcb29fc9bd0a79acb505fc75a5bac71
Ashley-887 <149943049+Ashley-887@users.noreply.github.com>
2023-11-26 22:03:56 +0800

    数组三

--- text3.c
+++ text3.c
@@ -1,24 +1,15 @@
+//在两个字符数组中实现字符串连接
+//把s2数组中值为'\0'的元素前的所有字符拷贝到s1末尾
 #include<stdio.h>
-#include<stdlib.h>
-int peach(int a,int b)
-{
-    if(a==0)
-    {
-        printf("%d",b);
-    }
-    else
-    {
-        a--;
-        b=(b+1)*2;
-        return peach(a,b);
-    }
-}
-
 int main()
 {
-    int a,b;
-    printf("第a天吃完后剩b个桃子:");
-    scanf("%d%d",&a,&b); 
-    peach(a,b);
-    return 0;
+   char s1[10]={"hi"},s2[10]={"zhao"};
+   int i,j;
+   while(s1[i]!='\0')
+     i++;
+   while(s2[j]!='\0')
+     s1[i++]=s2[j++];
+     s1[i]='\0';
+     puts(s1);
+     //printf("%s",s1);
 }
