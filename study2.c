014ba5b1b3558e651e432b72bb883880124e7d61
Ashley-887 <149943049+Ashley-887@users.noreply.github.com>
2023-12-03 21:46:38 +0800

    结构体2

--- study2.c
+++ study2.c
@@ -1,80 +1,16 @@
 #include<stdio.h>
+//共用体
+// union item//存储空间取决于最大变量，重叠占用内存空间
+// {
+//     int n;
+//     float x;
+//     char c;
+// }code;
 
-//put(s)或printf("%s",s)
-//Notice:用来一次输入全部字符
-//一定要确保数组s中储存的是一个字符串
-//是"hello" 不是'h''e''l''l''o''
-
-//Notice:单个字符输出用putchar(s[i])或printf("%c",s[i])
-//需要编制循环实现输出
-//char s[255]={"hello"};  int i;
-//while(s[i]!='\0')
-//{
-    //putchar(s[i]); i++;
-//}
-
-// Notice:字符串拷贝（将字符串从一个常量或数组中拷贝到另一个数组）
-// 调用系统库函数strcpy(char s1[],char s2[])或自己编写
-// 使用strcpy要包含<string.h>
-// s1长度>s2 s2必须为一字符串
-//一个汉字占两个字节，即相当于两个字母
-
-//Notice:
+// //初始化，与第一个变量类型一致
+// union item code={100};
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// code.m=123;
+// code.x=123.45;
+// printf("%d",code.m)//code.m已被覆盖，只有.x的长度
