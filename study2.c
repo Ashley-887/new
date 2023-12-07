f5f88e2dce8d3e92b76f5bd287a67fedef394370
Ashley-887 <149943049+Ashley-887@users.noreply.github.com>
2023-12-07 21:29:34 +0800

    指针2

--- study2.c
+++ study2.c
@@ -1,16 +1,60 @@
-#include<stdio.h>
-//共用体
-// union item//存储空间取决于最大变量，重叠占用内存空间
-// {
-//     int n;
-//     float x;
-//     char c;
-// }code;
+ #include<stdio.h>
+//指针类型
+//char*  int* float* int(*a)[n] double*
 
-// //初始化，与第一个变量类型一致
-// union item code={100};
+//初始化
+//1.空地址NULL int*pa=NULL
+//2.已声明的变量或数组的地址 int x=1;
+                         //int *px=&x;
 
+//*与&为互逆运算
+//int a=10,b=0;
+//b=*(&a);//即b=a
 
-// code.m=123;
-// code.x=123.45;
-// printf("%d",code.m)//code.m已被覆盖，只有.x的长度
+//利用指针变量处理数据
+int main()
+{
+    double x=2;
+    double*px=&x;
+    scanf("%lf",px);
+    printf("%lf",x);
+    return 0;
+}
+
+//指针的指针(二级指针)
+int **ppa,*pa,a;
+pa=&a;
+ppa=&pa;
+
+int main()
+{
+    int a=10;
+    int *pa=&a;
+    int **ppa=&pa;
+
+    *pa=100;//地址pa加*返回到a，即a=100
+    **ppa=200;//*ppa为pa，再结合剩下的*得到a
+    return 0;
+}
+
+//指针类型函数
+int main()
+{
+    long*p;
+    long score[5]={1,2,3,4,5};
+    p=GetMax(score);
+    printf("%d",*p);
+    return 0;
+}
+long *GetMax(long*score)
+{
+    int max=score[0];
+    for(int i=1;i<5;i++){
+        if(score[i]>max){
+            max=score[i];
+        }
+    }
+    return max;
+}
+
+//指针与数组
